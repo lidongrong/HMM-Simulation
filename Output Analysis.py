aaec49f2d9b02b7cead60be8f5edaf83619cbcc5
lidongrong <32429197+lidongrong@users.noreply.github.com>
2021-10-12 15:14:42 +0800

    Update Output Analysis.py

--- Output Analysis.py
+++ Output Analysis.py
@@ -1,15 +1,84 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Oct  2 20:50:34 2021
+Created on Mon Oct 11 14:01:02 2021
 
-@author: a
+@author: s1155151972
 """
 
-b1=[]
-
-for x in b:
-    b1.append(x[0][2])
 
 import matplotlib.pyplot as plt
-plt.hist(b1,bins=50)
-plt.show()
+
+# draw posterior of a11
+dist=[]
+
+for x in post_A:
+    dist.append(x[0][0])
+
+plt.xlabel(f"a11 with missing rate p=0.4 \n 95% percentile:\n ({np.percentile(dist,2.5)},\n {np.percentile(dist,97.5)})")
+plt.ylabel("frequency")
+plt.text(0.38,0,f'95% percentile:\n ({np.percentile(dist,2.5)},\n {np.percentile(dist,97.5)})')
+plt.hist(dist,bins=60)
+
+
+dist=[]
+for x in post_A:
+    dist.append(x[1][1])
+
+plt.xlabel(f"a22 with missing rate p=0.4 \n 95% percentile:\n ({np.percentile(dist,2.5)},\n {np.percentile(dist,97.5)})")
+plt.ylabel("frequency")
+
+plt.hist(dist,bins=60)
+
+dist=[]
+for x in post_A:
+    dist.append(x[2][2])
+
+plt.xlabel(f"a33 with missing rate p=0.4 \n 95% percentile:\n ({np.percentile(dist,2.5)},\n {np.percentile(dist,97.5)})")
+plt.ylabel("frequency")
+#plt.text(0.55,0,f'95% percentile:\n ({np.percentile(dist,2.5)},\n {np.percentile(dist,97.5)})')
+plt.hist(dist,bins=60)
+
+dist=[]
+for x in post_A:
+    dist.append(x[3][3])
+
+plt.xlabel(f"a44 with missing rate p=0.4 \n 95% percentile:\n ({np.percentile(dist,2.5)},\n {np.percentile(dist,97.5)})")
+plt.ylabel("frequency")
+#plt.text(0.44,0,f'95% percentile:\n ({np.percentile(dist,2.5)},\n {np.percentile(dist,97.5)})')
+plt.hist(dist,bins=60)
+
+dist=[]
+for x in post_B:
+    dist.append(x[0][0])
+
+plt.xlabel(f"b11 with missing rate p=0.4 \n 95% percentile:\n ({np.percentile(dist,2.5)},\n {np.percentile(dist,97.5)})")
+plt.ylabel("frequency")
+#plt.text(0.57,0,f'95% percentile:\n ({np.percentile(dist,2.5)},\n {np.percentile(dist,97.5)})')
+plt.hist(dist,bins=60)
+
+dist=[]
+for x in post_B:
+    dist.append(x[1][3])
+
+plt.xlabel(f"b24 with missing rate p=0.4 \n 95% percentile:\n ({np.percentile(dist,2.5)},\n {np.percentile(dist,97.5)})")
+plt.ylabel("frequency")
+#plt.text(0.3,0,f'95% percentile:\n ({np.percentile(dist,2.5)},\n {np.percentile(dist,97.5)})')
+plt.hist(dist,bins=60)
+
+dist=[]
+for x in post_B:
+    dist.append(x[2][4])
+
+plt.xlabel(f"b35 with missing rate p=0.4 \n 95% percentile:\n ({np.percentile(dist,2.5)},\n {np.percentile(dist,97.5)})")
+plt.ylabel("frequency")
+#plt.text(0,0,f'95% percentile:\n ({np.percentile(dist,2.5)},\n {np.percentile(dist,97.5)})')
+plt.hist(dist,bins=60)
+
+dist=[]
+for x in post_B:
+    dist.append(x[4][4])
+
+plt.xlabel(f"b55 with missing rate p=0.4 \n 95% percentile:\n ({np.percentile(dist,2.5)},\n {np.percentile(dist,97.5)})")
+plt.ylabel("frequency")
+#plt.text(0.62,0,f'95% percentile:\n ({np.percentile(dist,2.5)},\n {np.percentile(dist,97.5)})')
+plt.hist(dist,bins=60)
