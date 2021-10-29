56fbfe7e5850e293b57653d91720ee810de21316
lidongrong <32429197+lidongrong@users.noreply.github.com>
2021-10-29 14:41:33 +0800

    Update Obs_Gibbs.py

--- Obs_Gibbs.py
+++ Obs_Gibbs.py
@@ -10,7 +10,7 @@ import numpy as np
 import HMM
 import Sampling
 import time
-from multiprocessing import pool
+from multiprocessing import Pool
 import scipy.stats as stats
 
 
@@ -125,7 +125,10 @@ def sample_B(data,I,B):
         n4=np.sum(np.logical_and(I==HMM.hidden_state[j],data==HMM.obs_state[3]))
         n5=np.sum(np.logical_and(I==HMM.hidden_state[j],data==HMM.obs_state[4]))
         B[j,:]=np.random.dirichlet((1+n1,1+n2,1+n3,1+n4,1+n5),1)[0]
-    return B
+    B=np.array(B)
+    new_B=B
+    #print(B)
+    return new_B
 
 # Sample A out using Metropolis within Gibbs
 # Algorithm based on Resarch note in 2021.10.28
@@ -134,57 +137,147 @@ def sample_A(data,I,A):
     # Because we assume the last state is an absorbing state
     for j in range(0,A.shape[0]-1):
         # randomly choose a batch for estimation
-        batch_size=5
-        batch=I[np.random.choice(I.shape[0],batch_size),:]
-        indexer=np.where(batch!='None')
+        # estimation via all data will cause the prob decay to 0
+        batch_size=15
+        batch=I[np.random.choice(I.shape[0],batch_size,replace=False),:]
+        
         
         # acquire the parameter from the last iteration
         a=A[j,j]
+        
         # sample from proposal distribution
-        new_a=np.random.beta(2,(2/a-2),1)[0]
+        # prevent b from <0, so if b too small, truncate by .0000001
+        new_a=np.random.beta(2,max(2/a-2,0.0000001),1)[0]
+        #new_a=np.random.uniform(0,1,1)[0]
         new_A=A.copy()
         new_A[j,j]=new_a
+        new_A[j,j+1]=1-new_a
+        
         # compute the likelihood up to a normalizing constant
         p=1
-        p_new=1
+        new_p=1
         for k in range(0,batch_size):
             indexer=np.where(batch[k]!='None')[0]
-            # We always assume start from a specific state
-            if j==0 and indexer[0]!=0:
-                pos=np.where(HMM.hidden_state==batch[k][indexer[0]])[0][0]
-                p=p*np.linalg.matrix_power(A,indexer[0])[j][pos]
-                p_new=p_new*np.linalg.matrix_power(new_A,indexer[0])[j][pos]
+            # the case that the whole sequence is missing
+            if indexer.size!=0:
+            
+                # We always assume start from a specific state
+                if j==0 and indexer[0]!=0:
+                    pos=np.where(HMM.hidden_state==batch[k][indexer[0]])[0][0]
+                    p=p*np.linalg.matrix_power(A,indexer[0])[j][pos]
+                    new_p=new_p*np.linalg.matrix_power(new_A,indexer[0])[j][pos]
                 
             
             
-            for i in range(0,len(indexer)-1):
-                if batch[k][indexer[i]]==HMM.hidden_state[j] and batch[k][indexer[i]]==batch[k][indexer[i+1]]:
-                    p=p*np.linalg.matrix_power(A,indexer[i+1]-indexer[i])[j][j]
-                    p_new=p_new*np.linalg.matrix_power(new_A,indexer[i+1]-indexer[i])[j][j]
-                if batch[k][indexer[i]]==HMM.hidden_state[j] and batch[k][indexer[i]]!=batch[k][indexer[i+1]]:
-                    pos=np.where(HMM.hidden_state==batch[k][indexer[i+1]])[0][0]
-                    p=p*np.linalg.matrix_power(A,indexer[i+1]-indexer[i])[j][pos]
-                    p_new=p_new*np.linalg.matrix_power(new_A,indexer[i+1]-indexer[i])[j][pos]
-                    break
+                for i in range(0,len(indexer)-1):
+                    if batch[k][indexer[i]]==HMM.hidden_state[j] and batch[k][indexer[i]]==batch[k][indexer[i+1]]:
+                        p=p*np.linalg.matrix_power(A,indexer[i+1]-indexer[i])[j][j]
+                        new_p=new_p*np.linalg.matrix_power(new_A,indexer[i+1]-indexer[i])[j][j]
+                    if batch[k][indexer[i]]==HMM.hidden_state[j] and batch[k][indexer[i]]!=batch[k][indexer[i+1]]:
+                        pos=np.where(HMM.hidden_state==batch[k][indexer[i+1]])[0][0]
+                        p=p*np.linalg.matrix_power(A,indexer[i+1]-indexer[i])[j][pos]
+                        new_p=new_p*np.linalg.matrix_power(new_A,indexer[i+1]-indexer[i])[j][pos]
+                        break
+        
         
         # determine the metropolis acceptance rate
+        # avoid dominator=0
         if p==0:
             u=1
         else:
-            r=(p_new*stats.beta.pdf(new_a,(2/a-2),1))/(p*stats.beta.pdf(a,(2/new_a-2),1))
+            
+            #r= acceptance rate
+            r=(new_p*stats.beta.pdf(a,2,max((2/new_a-2),0.0000001)))/(p*stats.beta.pdf(new_a,2,max((2/a-2),0.0000001)))
+            #r=new_p/p
             u=min(1,r)
         
         unif=np.random.uniform(0,1,1)[0]
         if unif<u:
             A[j,j]=new_a
-            A[j,j+1]=1-A[j,j]
-    return A
-            
+            A[j,j+1]=1-new_a
+    
+    A=np.array(A)
+    
+    final_A=A
+    
+    return final_A
+
+# Gibbs sampling using Metropolis within Gibbs algorithm
+# input I,A,B: initial guesses of the parameter
+# n: number of samples to draw
+def Gibbs(data,I,A,B,n):
+    post_A=[]
+    post_B=[]
+    for i in range(0,n):
+        print(i)
+        A=sample_A(data,I,A)
+        B=sample_B(data,I,B)
+        I=sample_latent_seq(data,I,A,B)
+        
+        post_A.append(A)
+        post_B.append(B)
+    post_A=np.array(post_A)
+    post_B=np.array(post_B)
+    return post_A,post_B
+
+# Gibbs sampling using Metropolis within Gibbs algorithm (acceleration by parallel computing)
+# input I,A,B: initial guesses of the parameter
+# n: number of samples to draw
+# p: Pool
+def parallel_Gibbs(data,I,A,B,n):
+    post_A=[]
+    post_B=[]
+    for i in range(0,n):
+        print(i)
+        A=sample_A(data,I,A)
+        B=sample_B(data,I,B)
+        
+        I=p.starmap(sample_latent_seq,[(data[0:450,:],I[0:450,:],A,B),(data[450:900,:],
+                                                                     I[450:900,:],A,B),(data[900:1350,:],
+                                                                                         I[900:1350,:],A,B),
+                                                                                         (data[1350:1800,:],
+                                                                                          I[1350:1800,:],A,B),
+                                                                                         (data[1800:2250,:],
+                                                                                          I[1800:2250,:],A,B),
+                                                                                         (data[2250:2700,:],
+                                                                                          I[2250:2700,:],A,B),
+                                                                                         (data[2700:3150,:],
+                                                                                          I[2700:3150,:],A,B),
+                                                                                         (data[3150:3600,:],
+                                                                                          I[3150:3600,:],A,B)])
+                                                                                        
+        I=np.vstack((I[0],I[1],I[2],I[3],I[4],I[5],I[6],I[7]))
+        #I=sample_latent_seq(data,I,A,B)
+        
+        
+        
+        post_A.append(A)
+        post_B.append(B)
+    post_A=np.array(post_A)
+    post_B=np.array(post_B)
+    return post_A,post_B
+        
                     
 if __name__=='__main__':
     A,B,data,I=initialize()
+    
+    #post_A,post_B=Gibbs(data,I,A,B,8000)
+    
+    p=Pool(8)
+    post_A,post_B=parallel_Gibbs(data,I,A,B,5000)
+    
+    
     print('Program finished')
+    
+
         
+        
+
+
+
+    
+
+
         
 
 
