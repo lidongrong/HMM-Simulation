c13536d1945c65663ab0266088d6ea6f1804d5e6
lidongrong <32429197+lidongrong@users.noreply.github.com>
2021-12-23 16:29:01 +0800

    Add files via upload

---
+++ Collaboration/OpenData.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import pandas as pd
+
+Px=pd.read_parquet('Px.parquet')
+Dx=pd.read_parquet('Dx.parquet')
+Lab=pd.read_parquet('Lab.parquet')
+Med=pd.read_parquet('Med.parquet')
+
