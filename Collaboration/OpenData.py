cd51bcfbc6b1b64a06f39cf84ddf6ab677eced39
lidongrong <32429197+lidongrong@users.noreply.github.com>
2021-12-27 18:14:58 +0800

    Update OpenData.py

--- Collaboration/OpenData.py
+++ Collaboration/OpenData.py
@@ -12,3 +12,7 @@ Dx=pd.read_parquet('Dx.parquet')
 Lab=pd.read_parquet('Lab.parquet')
 Med=pd.read_parquet('Med.parquet')
 
+Px.to_csv('Px.csv')
+Dx.to_csv('Dx.csv')
+Lab.to_csv('Lab.csv')
+Med.to_csv('Med.csv')
