675f9bcd19a66b90bd8392887ad63480cc0b1173
lidongrong <32429197+lidongrong@users.noreply.github.com>
2022-02-28 17:04:56 +0800

    Add files via upload

---
+++ EM-HMM/Test_Viterbi.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  7 14:23:10 2022
+
+@author: s1155151972
+"""
+import numpy as np
+from ZMARGibbs import*
+from HMM import*
+from SeqSampling import*
+
+transition=np.array([[0.6,0.3,0.1],[0.1,0.6,0.3],[0.3,0.1,0.6]])
+state=np.array(['0','1','2'])
+hidden_state=state
+obs_state=np.array(['Blue','Green','Yellow'])
+obs_prob=np.array([[0.7,0.2,0.1],
+                   [0.1,0.7,0.2],
+                   [0.2,0.1,0.7]
+    ])
+
+pi=np.array([0.7,0.2,0.1])
+
+def viterbi(y, A, B, Pi,hidden_state,obs_state):
+    """
+    Return the MAP estimate of state trajectory of Hidden Markov Model.
+
+    Parameters
+    ----------
+    y : array (T,)
+        Observation state sequence. int dtype.
+    A : array (K, K)
+        State transition matrix. See HiddenMarkovModel.state_transition  for
+        details.
+    B : array (K, M)
+        Emission matrix. See HiddenMarkovModel.emission for details.
+    Pi: optional, (K,)
+        Initial state probabilities: Pi[i] is the probability x[0] == i. If
+        None, uniform initial distribution is assumed (Pi[:] == 1/K).
+
+    Returns
+    -------
+    x : array (T,)
+        Maximum a posteriori probability estimate of hidden state trajectory,
+        conditioned on observation sequence y under the model parameters A, B,
+        Pi.
+    T1: array (K, T)
+        the probability of the most likely path so far
+    T2: array (K, T)
+        the x_j-1 of the most likely path so far
+    """
+    # Cardinality of the state space
+    K = A.shape[0]
+    # Initialize the priors with default (uniform dist) if not given by caller
+    Pi = Pi if Pi is not None else np.full(K, 1 / K)
+    T = len(y)
+    indexer=np.where(y!='None')[0]
+    T1 = np.empty((K, T), 'd')
+    T2 = np.empty((K, T), 'B')
+
+    # Initilaize the tracking tables from first observation
+    
+    if y[0]!='None':
+        T1[:, 0] = Pi * B[:, np.where(obs_state==y[0])[0][0]]
+        T2[:, 0] = 0
+    if y[0]=='None':
+        T1[:, 0] = Pi
+        T2[:, 0] = 0
+    
+    # Iterate throught the observations updating the tracking tables
+    for i in range(1, T):
+        if i in indexer:
+            T1[:, i] = np.max(T1[:, i - 1] * A.T * B[np.newaxis, :, np.where(obs_state==y[i])[0][0]].T, 1)
+            T2[:, i] = np.argmax(T1[:, i - 1] * A.T, 1)
+        else:
+            T1[:, i] = np.max(T1[:, i - 1] * A.T,1)
+            T2[:, i] = np.argmax(T1[:, i - 1] * A.T, 1)
+
+    # Build the output, optimal model trajectory
+    x = np.empty(T,'B')
+    x[-1] = np.argmax(T1[:, T - 1])
+    for i in reversed(range(1, T)):
+        x[i - 1] = T2[x[i], i]
+    
+    
+    x=np.array([hidden_state[i] for i in x])
+    
+    return x
+
+
+def vtb(y,A,B,pi,hidden_state,obs_state):
+    n=y.shape[0]
+    z=[]
+    for i in range(0,n):
+        new_z=viterbi(y[i],A,B,pi,hidden_state,obs_state)
+        z.append(new_z)
+    z=np.array(z)
+    return z
+
+# random guess
+def random_guesser(data,hidden_state):
+    n=data.shape[0]
+    t=data.shape[1]
+    guess=[]
+    for i in range(0,n):
+        new_guess=np.random.choice(hidden_state,t)
+        guess.append(new_guess)
+    
+    guess=np.array(guess)
+    
+    return guess
+
+
